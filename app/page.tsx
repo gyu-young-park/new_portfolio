e39732300f43a5e8350f6c47e4ae892bc2fdb7b8
gyu-young-park <gyoue200125@gmail.com>
2024-05-31 01:28:45 +0900

    feat: add client cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Clients from "@/components/Clients";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProject from "@/components/RecentProject";
@@ -13,6 +14,7 @@ export default function Home() {
         <Hero/>
         <Grid/>
         <RecentProject/>
+        <Clients/>
       </div>
     </main>
   );
--- data/index.ts
+++ data/index.ts
@@ -74,7 +74,7 @@ export const navItems = [
       id: 1,
       title: "3D Solar System Planets to Explore",
       des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-      img: "/p1.svg",
+      img: "https://www.clipartmax.com/png/small/111-1113072_a-golang-gopher.png",
       iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
       link: "https://github.com/adrianhajdin?tab=repositories",
     },
