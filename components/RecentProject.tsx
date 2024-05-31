948113cd33162a93a2f2ad0c6bf65e9f00edb43d
gyu-young-park <gyoue200125@gmail.com>
2024-06-01 00:24:08 +0900

    fix: correct type

--- components/RecentProject.tsx
+++ components/RecentProject.tsx
@@ -10,9 +10,9 @@ const RecentProject = () => {
             A small selection of {' '}
             <span className='text-purple'>recent projects</span>
         </h1>
-        <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10'>
+        <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-16 mt-10'>
             {projects.map(({id, title, des, img, iconLists, link}) => (
-                <div key={id} className="sm:h-[41rem] h-[32tem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]">
+                <div key={id} className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]">
                     <PinContainer title={title} href={link} >
                         <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10'>
                             <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
